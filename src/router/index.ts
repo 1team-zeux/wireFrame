43996e26f98d08d246dacebddf996696e8376e49
ljsuuuu <ljsuu1346@gmail.com>
2026-05-21 11:04:30 +0900

    기능에 따른 파일명 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Page1 from '../views/Page1.vue'
-import Page2 from '../views/Page2.vue'
-import Page3 from '../views/Page3.vue'
-import Page4 from '../views/Page4.vue'
-import Page5 from '../views/Page5.vue'
-import Page6 from '../views/Page6.vue'
-import Page7 from '../views/Page7.vue'
-import Page8 from '../views/Page8.vue'
+import SlaUpload from '../views/SlaUpload.vue'
+import SlaWizard from '../views/SlaWizard.vue'
+import TopologySelect from '../views/TopologySelect.vue'
+import PolicyConfig from '../views/PolicyConfig.vue'
+import TerraformApply from '../views/TerraformApply.vue'
+import CustomerDashboard from '../views/CustomerDashboard.vue'
+import CustomerSlo from '../views/CustomerSlo.vue'
+import ServiceMonitor from '../views/ServiceMonitor.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/page1' },
-    { path: '/page1', component: Page1 },
-    { path: '/page2', component: Page2 },
-    { path: '/page3', component: Page3 },
-    { path: '/page4', component: Page4 },
-    { path: '/page5', component: Page5 },
-    { path: '/page6', component: Page6 },
-    { path: '/page7', component: Page7 },
-    { path: '/page8', component: Page8 },
+    { path: '/page1', component: SlaUpload },
+    { path: '/page2', component: SlaWizard },
+    { path: '/page3', component: TopologySelect },
+    { path: '/page4', component: PolicyConfig },
+    { path: '/page5', component: TerraformApply },
+    { path: '/page6', component: CustomerDashboard },
+    { path: '/page7', component: CustomerSlo },
+    { path: '/page8', component: ServiceMonitor },
   ],
 })
 
